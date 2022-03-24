e4f1f68a6402d6e65e3d0b01dc811120c24c6a68
RaduADumitru <radu.dumitru@rocketmail.com>
2022-03-24 22:47:36 +0200

    Added DbManager

---
+++ qt_to-do/sqlite/dbmanager.h
@@ -0,0 +1,17 @@
+#ifndef DBMANAGER_H
+#define DBMANAGER_H
+#include <QSql>
+#include <QSqlDatabase>
+
+class DbManager
+{
+public:
+    DbManager(const QString& path);
+    bool addTask(const QString& name, const QString& description);
+    bool taskExists(const int id);
+    bool deleteTask(const int id);
+private:
+    QSqlDatabase m_db;
+};
+
+#endif // DBMANAGER_H
