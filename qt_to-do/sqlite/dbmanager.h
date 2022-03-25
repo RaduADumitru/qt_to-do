417fd5b3a1a77c00a815bc4eaf7cc44daab58d45
Radu Dumitru <72195132+RaduADumitru@users.noreply.github.com>
2022-03-25 18:25:31 +0200

    Update dbmanager.h

--- qt_to-do/sqlite/dbmanager.h
+++ qt_to-do/sqlite/dbmanager.h
@@ -10,11 +10,7 @@ public:
     bool taskExists(const int id);
     bool deleteTask(const int id);
     QVector<QVector<QString>> getAll();
-<<<<<<< HEAD
-
     static DbManager* getInstance();
-=======
->>>>>>> b93ce82c7ce897a493ba65edfdce338dd6ea85cf
 private:
     QSqlDatabase m_db;
     static DbManager* s_instance;
